f4bf080c4c7297017886bb8d9497b33a8939b7b7
hrvoje32 <henrygasparic992@icloud.com>
2024-06-04 02:24:09 +0100

    Delete Quizz_App directory

--- Quizz_App/fetch_leaderboard.php
+++
@@ -1,8 +0,0 @@
-<?php
-require 'db_connect.php';
-
-$stmt = $pdo->query("SELECT username, score FROM users JOIN scores ON users.id = scores.user_id ORDER BY score DESC LIMIT 5");
-$leaderboard = $stmt->fetchAll(PDO::FETCH_ASSOC);
-
-echo json_encode($leaderboard);
-?>
